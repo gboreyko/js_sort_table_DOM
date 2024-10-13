b118556cf0501c95c32b4e390cdbc51fb83ea757
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2024-10-13 18:01:22 +0300

    made three changes enhancing the performance of my code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,34 @@
 'use strict';
 
 const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
 
 function convertToNumber(currency) {
-  const str = currency.slice(1);
-  let result = '';
-
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === ',') {
-      result += '.';
-    } else {
-      result += str[i];
-    }
-  }
-
-  return +result;
+  return Number(currency.split(',').join('.').slice(1));
 }
 
 thead.addEventListener('click', (e) => {
   const title = e.target.textContent;
   const index = e.target.cellIndex;
 
-  const tbody = document.querySelector('tbody');
   const rows = [...tbody.rows];
+  const enlargedRows = rows.map((row) => [row.cells[index].textContent, row]);
 
-  rows.sort((rowA, rowB) => {
-    const valueA = rowA.cells[index].textContent;
-    const valueB = rowB.cells[index].textContent;
-
+  enlargedRows.sort((row1, row2) => {
     switch (title) {
       case 'Name':
       case 'Position':
-        return valueA.localeCompare(valueB);
+        return row1[0].localeCompare(row2[0]);
 
       case 'Age':
-        return valueA - valueB;
+        return row1[0] - row2[0];
 
       case 'Salary':
-        return convertToNumber(valueA) - convertToNumber(valueB);
+        return convertToNumber(row1[0]) - convertToNumber(row2[0]);
     }
   });
 
-  tbody.append(...rows);
+  const sortRows = enlargedRows.map((item) => item[1]);
+
+  tbody.append(...sortRows);
 });
